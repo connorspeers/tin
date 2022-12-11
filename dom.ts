40c76feed795f9540e989676e005c4efe0a69d69
Connor Speers <connor@speers.dev>
2022-12-11 09:55:34 -0500

    dom.ts

---
+++ dom.ts
@@ -0,0 +1,8 @@
+// Copyright 2022 Connor Speers. All rights reserved. MIT License.
+// This module is browser-only.
+
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="esnext" />
